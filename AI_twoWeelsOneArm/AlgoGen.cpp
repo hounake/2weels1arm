9a43a87e8e7aea08f9a9c83c7b7d7d53ff6d3535
montre_n <montre_n@git.epitech.eu>
2016-10-08 10:22:04 +0200

    create project

---
+++ AI_twoWeelsOneArm/AlgoGen.cpp
@@ -0,0 +1,20 @@
+#include "AlgoGen.h"
+
+AlgoGen::AlgoGen();
+AlgoGen::~AlgoGen();
+
+int	AlgoGen::start() {
+
+}
+
+int AlgoGen::handle() {
+
+}
+
+void AlgoGen::loadGeneration() {
+
+}
+
+void play() {
+
+}
