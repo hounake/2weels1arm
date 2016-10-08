9f8e878b3b609c259e9064cf440fd6778ebc8a14
montre_n <montre_n@git.epitech.eu>
2016-10-08 17:05:38 +0200

    pull fileConfig

--- 2Whells1Arm/Robot.cpp
+++ 2Whells1Arm/Robot.cpp
@@ -1,12 +1,76 @@
 #include "stdafx.h"
 #include "Robot.h"
 
-
 Robot::Robot()
+	: m_score(0), m_distance(0.0), m_selected(false) m_wrist(std::pair<simxInt, simxInt>(0, 0)), m_elbow(std::pair<simxInt, simxInt>(0, 0)), m_shoulder(std::pair<simxInt, simxInt>(0, 0))
 {
 }
 
-
 Robot::~Robot()
 {
+}
+
+int							getScore()
+{
+	return m_score;
+}
+
+float						getDistance()
+{
+	return m_distance;
+}
+
+bool						isSelected()
+{
+	return m_selected;
+}
+
+std::pair<simxInt, simxInt> &getWrist()
+{
+	return m_wrist;
+}
+
+std::pair<simxInt, simxInt> &getElbow()
+{
+	return m_elbow;
+}
+
+std::pair<simxInt, simxInt> &getShoulder()
+{
+	return m_shoulder;
+}
+
+void						setScore(int newScore)
+{
+	m_score = newScore;
+}
+
+void						setDistance(float newDist)
+{
+	m_distance = newDist;
+}
+
+void						select()
+{
+	m_select = true;
+}
+
+void						resetSelection()
+{
+	m_select = false;
+}
+
+void						setWrist(std::pair<simxInt, simxInt> &newWrist)
+{
+	m_wrist = newWrist;
+}
+
+void						setElbow(std::pair<simxInt, simxInt> &newElbow)
+{
+	m_elbow = newElbow;
+}
+
+void						setShoulder(std::pair<simxInt, simxInt> &newShoulder)
+{
+	m_shoulder = newShoulder;
 }
--- 2Whells1Arm/Robot.h
+++ 2Whells1Arm/Robot.h
@@ -1,8 +1,34 @@
 #pragma once
+
+#include <utility>
+#include "App.h"
+
 class Robot
 {
+	int							m_score;
+	float						m_distance;
+	bool						m_selected;
+	std::pair<simxInt, simxInt>	m_wrist;
+	std::pair<simxInt, simxInt>	m_elbow;
+	std::pair<simxInt, simxInt>	m_shoulder;
+
 public:
 	Robot();
 	~Robot();
+
+	int							getScore();
+	float						getDistance();
+	bool						isSelected();
+	std::pair<simxInt, simxInt> &getWrist();
+	std::pair<simxInt, simxInt> &getElbow();
+	std::pair<simxInt, simxInt> &getShoulder();
+
+	void						setScore();
+	void						setDistance();
+	void						select();
+	void						resetSelection();
+	void						setWrist();
+	void						setElbow();
+	void						setShoulder();
 };
 
