ce353e16516c123451d0a8baaef39456eb138e92
lefebv-z <marie.lefebvre@epitech.eu>
2016-10-14 14:12:00 +0100

    file manager

--- 2Whells1Arm/FileManager.cpp
+++ 2Whells1Arm/FileManager.cpp
@@ -10,3 +10,29 @@ FileManager::FileManager()
 FileManager::~FileManager()
 {
 }
+
+void FileManager::geneToLog()
+{
+}
+
+void FileManager::init(std::string & file)
+{
+}
+
+void FileManager::createConf()
+{
+}
+
+void FileManager::createDir()
+{
+}
+
+void FileManager::createGeneLog(std::map<int, std::vector<Robot>>& robots, int nbGene)
+{
+}
+
+void FileManager::CreateCSV()
+{
+}
+
+
--- 2Whells1Arm/FileManager.h
+++ 2Whells1Arm/FileManager.h
@@ -1,8 +1,21 @@
 #pragma once
+
+#include <map>
+#include <vector>
+#include "Robot.h"
+
 class FileManager
 {
 public:
 	FileManager();
 	~FileManager();
+
+	void geneToLog();
+	void init(std::string &file);
+
+	void createConf();
+	void createDir();
+	void createGeneLog(std::map<int, std::vector<Robot>> &robots, int nbGene);
+	void CreateCSV();
 };
 
