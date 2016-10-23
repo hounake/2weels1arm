ff89f7a81e72f2ce49f1794f5eaa8216b2d45545
montre_n <montre_n@git.epitech.eu>
2016-10-23 17:32:16 +0200

    branch made to avoid compiling problem (other's work has been commented)

--- 2Whells1Arm/FileManager.h
+++ 2Whells1Arm/FileManager.h
@@ -1,28 +1,28 @@
 #pragma once
 
-#include <map>
-#include <vector>
-#include "Robot.h"
-#include "LogConf.h"
-
-class FileManager
-{
-	std::string _fileName;
-	LogConf	_conf;
-	std::map<int, std::vector<Robot>> _lastReadGene;
-
-public:
-	FileManager();
-	~FileManager();
-
-	//void geneToLog();
-	//void init(std::string &file);
-
-	void createConf();
-	void createDir();
-	void createGeneLog(std::map<int, std::vector<Robot>> &robots, int nbGene);
-	//void createCSV();
-
-	std::map<int, std::vector<Robot>> &		initGeneFromFile(int nbGene);
-};
+//#include <map>
+//#include <vector>
+//#include "Robot.h"
+//#include "LogConf.h"
+//
+//class FileManager
+//{
+//	std::string _fileName;
+//	LogConf	_conf;
+//	std::map<int, std::vector<Robot>> _lastReadGene;
+//
+//public:
+//	FileManager();
+//	~FileManager();
+//
+//	//void geneToLog();
+//	//void init(std::string &file);
+//
+//	void createConf();
+//	void createDir();
+//	void createGeneLog(std::map<int, std::vector<Robot>> &robots, int nbGene);
+//	//void createCSV();
+//
+//	std::map<int, std::vector<Robot>> &		initGeneFromFile(int nbGene);
+//};
 
